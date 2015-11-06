80e1be1c6a3e27a0685c5dffc40f7f1e70ee43a6
Highstaker <heights999@yandex.ru>
2015-11-06 08:57:09 +0300

    v0.3.6
    +added check for zero-length replies in sendMessage (just in case I
    screw up)
    +wrapped all the processing of message in `try` to avoid bot crash. Now
    it drops a message that caused an error.

--- multitran_bot.py
+++ multitran_bot.py
@@ -5,7 +5,7 @@
 #-make donation info
 #-fix flags of languages in both help message and pick menu
 
-VERSION_NUMBER = (0,3,5)
+VERSION_NUMBER = (0,3,6)
 
 import logging
 import telegram
@@ -155,13 +155,14 @@ class TelegramBot():
 		logging.warning("Replying to " + str(chat_id) + ": " + text)
 		while True:
 			try:
-				self.bot.sendChatAction(chat_id,telegram.ChatAction.TYPING)
-				self.bot.sendMessage(chat_id=chat_id,
-					text=text,
-					parse_mode='Markdown',
-					disable_web_page_preview=(not preview),
-					reply_markup=telegram.ReplyKeyboardMarkup(key_markup)
-					)
+				if text:
+					self.bot.sendChatAction(chat_id,telegram.ChatAction.TYPING)
+					self.bot.sendMessage(chat_id=chat_id,
+						text=text,
+						parse_mode='Markdown',
+						disable_web_page_preview=(not preview),
+						reply_markup=telegram.ReplyKeyboardMarkup(key_markup)
+						)
 			except Exception as e:
 				if "Message is too long" in str(e):
 					self.sendMessage(chat_id=chat_id
@@ -219,137 +220,140 @@ class TelegramBot():
 			except KeyError:
 				self.subscribers[chat_id] = 1
 
-			if message:
-				if message == "/start":
-					self.sendMessage(chat_id=chat_id
-						,text=START_MESSAGE
-						)
-				elif message == "/help" or message == HELP_BUTTON:
-					self.sendMessage(chat_id=chat_id
-						,text=HELP_MESSAGE
-						)
-				elif message == "/about" or message == ABOUT_BUTTON:
-					self.sendMessage(chat_id=chat_id
-						,text=ABOUT_MESSAGE
-						)
-				elif message == "/rate" or message == RATE_ME_BUTTON:
-					self.sendMessage(chat_id=chat_id
-						,text=RATE_ME_MESSAGE
-						)
-				elif message == PICK_LANGUAGE_BUTTON:
-					self.sendMessage(chat_id=chat_id
-						,text="Select language"
-						,key_markup=LANGUAGE_PICK_KEY_MARKUP
-						)
-				elif message == BACK_BUTTON:
-					self.sendMessage(chat_id=chat_id
-						,text="Back to Main Menu"
-						)
-				elif message in list(LANGUAGE_INDICIES.keys()):
-					#message is a language pick
-					pass
-					self.subscribers[chat_id] = LANGUAGE_INDICIES[message]
-					self.sendMessage(chat_id=chat_id
-						,text="Language is set to " + message
-						)
-				else:
-					if message[0] == "/":
-						message = message[1:]
+			#I had no idea you could send an empty message
+			try:
+				if message:
+					if message == "/start":
+						self.sendMessage(chat_id=chat_id
+							,text=START_MESSAGE
+							)
+					elif message == "/help" or message == HELP_BUTTON:
+						self.sendMessage(chat_id=chat_id
+							,text=HELP_MESSAGE
+							)
+					elif message == "/about" or message == ABOUT_BUTTON:
+						self.sendMessage(chat_id=chat_id
+							,text=ABOUT_MESSAGE
+							)
+					elif message == "/rate" or message == RATE_ME_BUTTON:
+						self.sendMessage(chat_id=chat_id
+							,text=RATE_ME_MESSAGE
+							)
+					elif message == PICK_LANGUAGE_BUTTON:
+						self.sendMessage(chat_id=chat_id
+							,text="Select language"
+							,key_markup=LANGUAGE_PICK_KEY_MARKUP
+							)
+					elif message == BACK_BUTTON:
+						self.sendMessage(chat_id=chat_id
+							,text="Back to Main Menu"
+							)
+					elif message in list(LANGUAGE_INDICIES.keys()):
+						#message is a language pick
+						pass
+						self.subscribers[chat_id] = LANGUAGE_INDICIES[message]
+						self.sendMessage(chat_id=chat_id
+							,text="Language is set to " + message
+							)
+					else:
+						if message[0] == "/":
+							message = message[1:]
 
-					page_url = 'http://www.multitran.ru/c/m.exe?l1='+str(self.subscribers[chat_id]) +'&s=' + message
-					page = getHTML_specifyEncoding(page_url, encoding='cp1251',method='replace')
-					soup = BeautifulSoup(page)
-
-					temp1 = [i for i in soup.find_all('table') if not i.has_attr('class') and not i.has_attr('id') and not i.has_attr('width') and i.has_attr('cellpadding') and i.has_attr('cellspacing') and i.has_attr('border') 
-					and not len(i.find_all('table'))
-					]
-
-					def process_result(temp1):
-						result = ""
-						for tr in temp1.find_all('tr'):
-							tds = tr.find_all('td')
-							def translations_row():
-								result = "_" + tr.find_all('a')[0].text + "_" + " "*5
-								for a in tr.find_all('a')[1:]:
-									if not 'i' in [i.name for i in a.children]:
-										result +=  a.text.replace("_","").replace("*","") + ";"
-								return result
-
-							if tds[0].has_attr('bgcolor'):
-								if tds[0]['bgcolor'] == "#DBDBDB":
-									result += "\n" + "*" + tr.text.split("|")[0].replace(tr.find_all('em')[0].text if tr.find_all('em') else "","").replace("в начало","").replace("\n","").replace("_","").replace("*","") + "*" + ( ( " "*5 + "_" + tr.find_all('em')[0].text  + "_") if tr.find_all('em') else "" )
-								else:
-									result += translations_row()
-							else:
-								result += translations_row()
-							result += "\n"
-						return result
-
-
-
-					result=""
-					#maybe the request is in Russian?
-					if not len(temp1):
-						page_url = 'http://www.multitran.ru/c/m.exe?l1=2&l2='+ str(self.subscribers[chat_id]) + '&s=' + message
+						page_url = 'http://www.multitran.ru/c/m.exe?l1='+str(self.subscribers[chat_id]) +'&s=' + message
 						page = getHTML_specifyEncoding(page_url, encoding='cp1251',method='replace')
 						soup = BeautifulSoup(page)
 
-						temp1 = [i for i in soup.find_all('table') if not i.has_attr('class') and not i.has_attr('id') and not i.has_attr('width') and i.has_attr('cellpadding') and i.has_attr('cellspacing') and i.has_attr('border') and not len(i.find_all('table'))]
+						temp1 = [i for i in soup.find_all('table') if not i.has_attr('class') and not i.has_attr('id') and not i.has_attr('width') and i.has_attr('cellpadding') and i.has_attr('cellspacing') and i.has_attr('border') 
+						and not len(i.find_all('table'))
+						]
 
-						# Maybe there is no such word?
+						def process_result(temp1):
+							result = ""
+							for tr in temp1.find_all('tr'):
+								tds = tr.find_all('td')
+								def translations_row():
+									result = "_" + tr.find_all('a')[0].text + "_" + " "*5
+									for a in tr.find_all('a')[1:]:
+										if not 'i' in [i.name for i in a.children]:
+											result +=  a.text.replace("_","").replace("*","") + ";"
+									return result
+
+								if tds[0].has_attr('bgcolor'):
+									if tds[0]['bgcolor'] == "#DBDBDB":
+										result += "\n" + "*" + tr.text.split("|")[0].replace(tr.find_all('em')[0].text if tr.find_all('em') else "","").replace("в начало","").replace("\n","").replace("_","").replace("*","") + "*" + ( ( " "*5 + "_" + tr.find_all('em')[0].text  + "_") if tr.find_all('em') else "" )
+									else:
+										result += translations_row()
+								else:
+									result += translations_row()
+								result += "\n"
+							return result
+
+
+
+						result=""
+						#maybe the request is in Russian?
 						if not len(temp1):
-							result="*Word not found!*"
-							varia = soup.find_all('td',string=re.compile("Варианты"))
-							print("varia",varia)
-							if varia:
-								logging.warning("Есть варианты замены!")
-								# print(varia[0].find_next_sibling("td").find_all('a'))
-								# quit()
-								result += "\n" + "*Possible replacements: *" + varia[0].find_next_sibling("td").text
+							page_url = 'http://www.multitran.ru/c/m.exe?l1=2&l2='+ str(self.subscribers[chat_id]) + '&s=' + message
+							page = getHTML_specifyEncoding(page_url, encoding='cp1251',method='replace')
+							soup = BeautifulSoup(page)
+
+							temp1 = [i for i in soup.find_all('table') if not i.has_attr('class') and not i.has_attr('id') and not i.has_attr('width') and i.has_attr('cellpadding') and i.has_attr('cellspacing') and i.has_attr('border') and not len(i.find_all('table'))]
+
+							# Maybe there is no such word?
+							if not len(temp1):
+								result="*Word not found!*"
+								varia = soup.find_all('td',string=re.compile("Варианты"))
+								print("varia",varia)
+								if varia:
+									logging.warning("Есть варианты замены!")
+									# print(varia[0].find_next_sibling("td").find_all('a'))
+									# quit()
+									result += "\n" + "*Possible replacements: *" + varia[0].find_next_sibling("td").text
+
+							else:
+								#request is in Russian
+								temp1= temp1[0]
+								result = process_result(temp1)
 
 						else:
-							#request is in Russian
+							#request is in foreign language
 							temp1= temp1[0]
 							result = process_result(temp1)
 
-					else:
-						#request is in foreign language
-						temp1= temp1[0]
-						result = process_result(temp1)
+						result += "\nLink to the dictionary page: " + page_url.replace(" ","+")
 
-					result += "\nLink to the dictionary page: " + page_url.replace(" ","+")
+						result += "\nCurrent language is " + list(LANGUAGE_INDICIES.keys())[list(LANGUAGE_INDICIES.values()).index(self.subscribers[chat_id]) ]
 
-					result += "\nCurrent language is " + list(LANGUAGE_INDICIES.keys())[list(LANGUAGE_INDICIES.values()).index(self.subscribers[chat_id]) ]
-
-					#break the result in several messages if it is too big
-					if len(result) < MAX_CHARS_PER_MESSAGE:
-						try:
-							self.sendMessage(chat_id=chat_id
-								,text=str(result)
-								,preview=False
-								)
-						except Exception as e:
-							logging.error("Could not process message. Error: " + str(e))
-					else:
-						result_split = result.split("\n")
-						result = ""
-						while result_split:
-							while True:
-								if result_split:
-									result += result_split.pop(0)+"\n"
-								else:
-									break
-
-								if len(result) > MAX_CHARS_PER_MESSAGE:
-									break
-
-							if result:
+						#break the result in several messages if it is too big
+						if len(result) < MAX_CHARS_PER_MESSAGE:
+							try:
 								self.sendMessage(chat_id=chat_id
 									,text=str(result)
 									,preview=False
 									)
-								result = ""
+							except Exception as e:
+								logging.error("Could not process message. Error: " + str(e))
+						else:
+							result_split = result.split("\n")
+							result = ""
+							while result_split:
+								while True:
+									if result_split:
+										result += result_split.pop(0)+"\n"
+									else:
+										break
 
+									if len(result) > MAX_CHARS_PER_MESSAGE:
+										break
+
+								if result:
+									self.sendMessage(chat_id=chat_id
+										,text=str(result)
+										,preview=False
+										)
+									result = ""
+			except Exception as e:
+				logging.error("Message processing failed! Error: " + str(e))
 			# Updates global offset to get the new updates
 			self.LAST_UPDATE_ID = update.update_id + 1
 
